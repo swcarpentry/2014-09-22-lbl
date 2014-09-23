adf25cefaaf3e44f2590462ef6c700ff181bb692
Matt Davis <jiffyclub@gmail.com>
2014-09-23 15:30:50 -0700

    filter_reservoir test

--- novice/sw_eng/test_res.py
+++ novice/sw_eng/test_res.py
@@ -14,3 +14,9 @@ def test_filter_reservoir():
     filename = 'reservoir_storage_small.dat'
     rows = res.read_reservoir_data(filename)
     abbr = 'SPB'
+
+    filtered_rows = res.filter_reservoir(rows, abbr)
+
+    assert len(filtered_rows) == 2
+    assert filtered_rows[0].abbr == abbr
+    assert filtered_rows[1].abbr == abbr
