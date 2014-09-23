c6db4df738f1d3d4b5feeb7f972247c2b6638d3f
Matt Davis <jiffyclub@gmail.com>
2014-09-23 15:52:35 -0700

    test filter_year

--- novice/sw_eng/test_res.py
+++ novice/sw_eng/test_res.py
@@ -20,3 +20,22 @@ def test_filter_reservoir():
     assert len(filtered_rows) == 2
     assert filtered_rows[0].abbr == abbr
     assert filtered_rows[1].abbr == abbr
+
+
+def test_filter_year():
+    filename = 'reservoir_storage_small.dat'
+    rows = res.read_reservoir_data(filename)
+    year = 2005
+
+    filtered_rows = res.filter_year(rows, year)
+
+    assert len(filtered_rows) == 3
+    for r in filtered_rows:
+        assert r.month[-4:] == str(year)
+
+    year = '2007'
+
+    filtered_rows = res.filter_year(rows, year)
+
+    assert len(filtered_rows) == 1
+    assert filtered_rows[0].month[-4:] == year
