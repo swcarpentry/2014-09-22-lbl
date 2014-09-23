ad634cda58b41a942b5e520bc9e6710d0dc0e20b
Matt Davis <jiffyclub@gmail.com>
2014-09-23 14:00:24 -0700

    read_reservoir_data function

--- novice/sw_eng/res.py
+++ novice/sw_eng/res.py
@@ -3,3 +3,23 @@ from collections import namedtuple
 ResRow = namedtuple(
     'ResRow',
     ('name', 'abbr', 'alt', 'lat', 'lon', 'county', 'month', 'storage'))
+
+
+def read_reservoir_data(filename):
+    rows = []
+
+    f = open(filename)
+    f.readline()
+
+    for line in f:
+        name,abbr,altitude,lat,lon,county,month,storage = line.split(',')
+        altitude = int(altitude)
+        lat = float(lat)
+        lon = float(lon)
+        storage = int(storage)
+        new_row = ResRow(name,abbr,altitude,lat,lon,county,month,storage)
+        rows.append(new_row)
+
+    f.close()
+
+    return rows
